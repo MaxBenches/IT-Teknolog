c6e244405645c9be1e49d8604eb58f1ad505812c
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-02-15 16:26:35 +0100

    intro to python

---
+++ IntroToPython.py
@@ -0,0 +1,8 @@
+#Writing a program in a text editor
+
+name = "Max"
+age = int("28")
+
+print("Hello IT-Technology student " + name + "!")
+print("I can see from your age that youy are born in the year: "+str(2023-age) + ".")
+print("A big welcome to you you " +name+" from Business Academy Aarhus!")
