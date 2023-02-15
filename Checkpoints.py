e48720aae82d0fca24ff03f2f8258b0559cbed57
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-02-15 16:57:51 +0100

    Checkpoints 2.7-2.9

---
+++ Checkpoints.py
@@ -0,0 +1,11 @@
+#Checkpoint 2.7 - Write a statement that displays youyr name
+
+#print("Max Bænk")
+
+#Checkpoint 2.8 - Write a statement that displays the following text "Python's the best!"
+
+#print("Python's the best!")
+
+#Checkpoint 2.9 - Write a statement that displays the following text "The cat said "meow."
+
+print('The cat said "meow".')
