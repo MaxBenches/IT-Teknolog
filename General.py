0f281c208d23953d1add4f945894eb1318c77c14
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-02-16 18:54:29 +0100

    ekstra kode om variabler

---
+++ General.py
@@ -0,0 +1,26 @@
+# Variabler 
+
+width = 10
+length = 5
+
+#Husk ingen "-tegn, ellers bliver variablen ikke referenced 
+#den printer ordene 'width' og 'length' i stedet
+
+#print(width)
+#print (length)
+
+#Variabler 
+#Hvad bliver følgende output?
+
+val = 99
+print("The value is", "val")
+
+#Outputtet bliver "The value is val" 
+#da val er sat innd som en streng
+#print funktionen kalder ikke variablen val
+#følgende kode vil give korrekt svar
+
+val = 99
+print("The value is", val)
+
+type(7)
