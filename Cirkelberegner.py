16234ceffeb8e9e3ea5170750b1a941086dcac7c
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-02-17 12:07:38 +0100

    Cirkelberegner

---
+++ Cirkelberegner.py
@@ -0,0 +1,6 @@
+# Input hvor stor radius er
+radius = float(input("Hvad er radius på din cirkel? "))
+# Udregn areal efter given radius
+PI = 3,1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679 
+areal = PI*radius**2.0
+print(areal)
