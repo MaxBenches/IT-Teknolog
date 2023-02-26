39a3b2c5c6a5df5330c244b1ffb7c0adfa004ee3
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-02-26 09:47:16 +0100

    Add 'Print' Assignment'

---
+++ 23.02.17 Øvelse - Print.py
@@ -0,0 +1,6 @@
+# Display name, adress, phone number and college major
+# Using f' string
+print(f'Max Bech\nDamagervej 12N\n28 40 34 50\nlærer')
+
+# Using concatenation
+print("Max Bech\n" + "Damagervej 12N\n" + "28 40 34 59\n" + "Lærer")
