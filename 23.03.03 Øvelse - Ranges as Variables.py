170de79a84cbce048b794e2cbde917ba5fe6afc0
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-03-03 11:24:25 +0100

    Updated - Ranges as Variables

--- 23.03.03 Øvelse - Ranges as Variables.py
+++ 23.03.03 Øvelse - Ranges as Variables.py
@@ -4,10 +4,12 @@ list_start = int(input("Which number would you like your list to start from? "))
 # Ask the user for an end value
 list_end = int(input("Which number would you like your list to end at? "))
 if list_end > list_start:
-    list_end = list_end + 1
+    #list_end = list_end + 1
+    list_end += 1               # Augmented Assignment Operators - Samme som kodelinjen over
 
 if list_end < list_start:
-    list_end = list_end - 1
+    #list_end = list_end - 1
+    list_end -= 1               # Augmented Assignment Operator - Samme som kodelinjen over
 
 # Ask the user for a step value
 list_steps = int(input("In what steps should the list ascend or descend? "))
