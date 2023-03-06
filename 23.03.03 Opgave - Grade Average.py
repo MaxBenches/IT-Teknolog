43753bfbb056fb25008d1e1c3c9f19dfa552b6cd
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-03-06 15:29:05 +0100

    Working, but unpolished Grade Averager

--- 23.03.03 Opgave - Grade Average.py
+++ 23.03.03 Opgave - Grade Average.py
@@ -1,11 +1,16 @@
-# Get amount of grades to be averaged
+# This program takes a user specified number of grades abd calculates the average
 
-LIST_START = 0
+LIST_START = 0                                                      # End of the loop
+list_end = int(input("How many grades need to be averaged? "))      # Start of the loop
 
-list_end = int(input("How many grades need to be averaged? "))
+# Start accumulator
+grade_total = 0
 
-# Loop to check if there are more grades
-
+# Get the grades from user and accumulate them
 for user_list in range(LIST_START, list_end):
-    grade_grade = int(input("What is the grade? "))
-    grade_total = grade_total + grade_grade
+    grade_number = int(input("Please enter a grade: "))
+    grade_total = grade_total + grade_number
+
+# Calculate average and display the average of grades
+grade_avg = grade_total/list_end
+print(f'The average grade is {grade_avg:.2f}.')
