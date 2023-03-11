ccd88d51daa9c378c718b4cbe0fe3adec66897ce
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-03-11 08:36:03 +0100

    Fully functional Rock, Paper, Scissors game

--- 23.03.10 Opgave - Rock Paper Scissor.py
+++ 23.03.10 Opgave - Rock Paper Scissor.py
@@ -1,34 +1,35 @@
 # Import randomisation library
 import random   # Imports random library
 
-# Assign a randomly generated number to the PC's choice of rock, paper or scissor
-pc_choice = (random.randrange(1,4))
-if pc_choice == 1:
-    pc_choice = "Rock"
-elif pc_choice == 2:
-    pc_choice = "Paper"
-elif pc_choice == 3:
-    pc_choice = "Scissors"
+# Placeholders to initialise "draw" loop
+pc_choice = 0
+player_choice = 0
 
-# Get player choice
-player_choice = input("Please enter your choice: Rock, Paper or Scissors\n")
-"""
-if pc_choice == "Rock" or "rock":
-    pc_choice = 1
-elif pc_choice == "Paper" or "paper":
-    pc_choice = 2
-elif pc_choice == "Scissors" or "Scissors":
-    pc_choice = 3
-"""
+# Loop to start over if pc and player chooses the same
+while pc_choice == player_choice:
+    # Assign a randomly generated number to the PC's choice of rock, paper or scissor
+    pc_choice = (random.randrange(1,4))
+    if pc_choice == 1:
+        pc_choice = "Rock"
+    elif pc_choice == 2:
+        pc_choice = "Paper"
+    elif pc_choice == 3:
+        pc_choice = "Scissors"
+    # Get player choice
+    player_choice = input("Please enter your choice: Rock, Paper or Scissors\n")
+    # Display draw message and start over
+    if pc_choice == player_choice:
+        print(f"PC and Player have both chosen {pc_choice}. It's a draw!\n"
+              f"Please play again to determine a winner.\n")
 
 # Display PC choice
 print(f'PC has chosen {pc_choice}')
 
-# Select winner
+# Select and display winner
 if pc_choice == "Rock" and player_choice == "Scissors":
-    print("Rock smahes Scissors. You lose!")
+    print("Rock smashes Scissors. You lose!")
 elif pc_choice == "Scissors" and player_choice == "Rock":
-    print("Rock smahes Scissors. You win!")
+    print("Rock smashes Scissors. You win!")
 elif pc_choice == "Scissors" and player_choice == "Paper":
     print("Scissors cut Paper. You lose!")
 elif pc_choice == "Paper" and player_choice == "Scissors":
@@ -37,6 +38,3 @@ elif pc_choice == "Paper" and player_choice == "Rock":
     print("Paper wraps Rock. You lose!")
 elif pc_choice == "Rock" and player_choice == "Paper":
     print("Paper wraps Rock. You win!")
-else:
-    print(f'PC and Player have both chosen {pc_choice}')
-# A while loop might be necessary with the above code nested inside?
