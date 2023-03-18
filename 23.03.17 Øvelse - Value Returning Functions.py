a263608de212a3fc8e00401b78e39d2cb0ba8a61
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-03-18 21:26:59 +0100

    Completed Value Returning Functions exercise

--- 23.03.17 Øvelse - Value Returning Functions.py
+++ 23.03.17 Øvelse - Value Returning Functions.py
@@ -1,19 +1,32 @@
+"""
+# This program takes two resistor values, calculates the
+# total resistance and prints it
+
+def main():
+    res1 = float(input("Please enter the value of the 1st resistor in ohms: "))
+    res2 = float(input("Please enter the value of the 2nd resistor in ohms: "))
+    calc_total_res(res1,res2)
+
+# Function to calculate and display the total resistance
+def calc_total_res(res1, res2):
+    total_res = 1/ ((1/res1)+(1/res2))
+    print(f"The total resistance is: {total_res} ohms")
+
+# Calling the main function
+main()
+"""
 
 
-# Fungerer ikke
-def get_res1():
-    res1 = float(input("Please input the value of Resistor 1 in ohms: "))
+# This program generates a random number
+# between 0 and 100, and displays it
+import random
 
-def get_res2():
-    res2 = float(input("Please input the value of Resistor 2 in ohms: "))
-
-def calc_res_total():
-    res_total = 1 / ((1/get_res1())+(1/get_res2()))
-    return res_total
-
-get_res1()
-get_res2()
-calc_res_total()
+def main():
+    random_number()
 
 
-print(res_total)
+def random_number():
+    number = random.randint(1,100)
+    print(f'Your randomly generated number is {number}.')
+
+main()
