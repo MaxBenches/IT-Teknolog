81be0552c7cdf4fbc1fe80245fb0b06b55e8d1cc
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-03-24 12:00:11 +0100

    Added new assignment 'Guess the number game'

---
+++ 23.03.17 Opgave - Guess the Number Game.py
@@ -0,0 +1,8 @@
+# Make a program that generates a random number between 1 and 100
+# Ask the player to guess the number
+# Tell the player if the number is higher or lower
+# Continue until the user guesses the right number
+# Print out the total number of guesses, this is the score and must be as low as possible.
+# You must use functions, ex. getRandomNumber(), checkNumber()
+# Create a flow chart of your program (one flow for each function)
+# Creat IPO charts for each function.
