583bb3f93fc6be1b7c42dcca4bd02ab2e157640e
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-03-24 11:52:05 +0100

    Started Odd-Even Counter exercise

--- 23.03.17 Øvelse - Odd-Even Counter.py
+++ 23.03.17 Øvelse - Odd-Even Counter.py
@@ -2,27 +2,30 @@
 # keep a count of how many of those random numbers are
 # even and how many of them are odd.
 
-
-# Virker ikke
 import random
 
 def main():
-    print(rand_numb_plus_count())
+    for i in range(1):
+        number = randNumber()
+        print(number)
+        evenOrOdd(number)
 
 
 
+# Generate a random number
+def randNumber():
+    randNum = random.randint(0,5)
+    return randNum
 
-def rand_numb_plus_count():
-    num_even = 0
-    num_odd = 0
-    for i in range(10):
-        num = print(random.randint(0,100))
-        if (num%2) == 0:
-            num_even =+ 1
-        else:
-            num_odd =+1
-    return num_odd + num_even
+# Check of number is even or odd
+def evenOrOdd(number):
+    if number % 2 == 0:
+        number = "Even"
+    else:
+        number = "Odd"
+    return number
 
+def countEvenOrOdd():
 
 
 main()
