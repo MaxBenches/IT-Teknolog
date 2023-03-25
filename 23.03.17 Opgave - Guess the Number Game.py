453acf68717f5ced39b5bb04eb7880ff57513104
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-03-25 21:11:14 +0100

    Started working on Assignment 'Guess the number'

--- 23.03.17 Opgave - Guess the Number Game.py
+++ 23.03.17 Opgave - Guess the Number Game.py
@@ -6,3 +6,32 @@
 # You must use functions, ex. getRandomNumber(), checkNumber()
 # Create a flow chart of your program (one flow for each function)
 # Creat IPO charts for each function.
+
+import random
+
+def main():
+    pcNum = getRandNum()
+    print(pcNum)
+    userGuess = 999
+    while pcNum != userGuess:
+        userGuess = getUserGuess()
+        checkGuess(pcNum,userGuess)
+
+# This function generates a random number
+def getRandNum():
+    randNum = random.randint(0,5)
+    return randNum
+
+# This function gets the user's guess
+def getUserGuess():
+    userGuess = int(input("Please enter your guess: "))
+    return userGuess
+
+# This function checks if the user's guess is higher or lower
+def checkGuess(pcNum,userGuess):
+    if userGuess < pcNum:
+        print("Your guess was lower. Please try again.")
+    elif userGuess > pcNum:
+        print("Your guess was higher. Please try again.")
+
+main()
