9db18e4850b631aa5409584e6d10a3d25a20fdd7
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-04-09 17:54:39 +0200

    Completed First Turtle Exercise

--- 23.02.17 Øvelse - Turtle and Input.py
+++ 23.02.17 Øvelse - Turtle and Input.py
@@ -13,24 +13,43 @@ def main():
     turtle.goto(getXCoord(),getYCoord())
     turtle.pendown()
     turtle.circle(getRadius())
+    print("Check the Turtle Graphics window to see your circle.")
     turtle.done()
 
 
 # This function prompts the user to input a diameter
 # It then halves this, so the turtle can use it as a radius
+# Also validates the input
 def getRadius():
-    userDiameter = int(input("What is the diameter of the circle you want drawn? "))
+    userDiameter = int(input("What is the diameter of the circle you want drawn?\n"
+                             "Please input a positive integer between 1 and 200\n"))
+    while userDiameter <= 0 or userDiameter > 100:
+        print("The diameter cannot be a negative integer or above 100.\n"
+              "Please enter a positive integer between 1 and 100.")
+        userDiameter = int(input("What is the diameter of the circle you want drawn?\n"))
     userRadius = userDiameter / 2
     return userRadius
 
 # This function prompts the user to input an X value for the turtle's starting point
+# Also validates the input
 def getXCoord():
-    userXCoord = int(input("Where would you like the circle to start on the X axis? "))
+    userXCoord = int(input("Where would you like the circle to start on the X axis?\n"
+                           "Please input a value between -100 and 100.\n"))
+    while userXCoord < -100 or userXCoord > 100:
+        print("The entered value is not in the specified range.\n"
+              "Please enter an integer between -100 and 100.")
+        userXCoord = int(input("Where would you like the circle to start on the X axis?\n"))
     return userXCoord
 
-# This function prompts the user to input an Y value for the turtle's starting point
+# This function prompts the user to input a Y value for the turtle's starting point
+# Also validates the input
 def getYCoord():
-    userYCoord = int(input("Where would you like the circle to start on the y axis? "))
+    userYCoord = int(input("Where would you like the circle to start on the Y axis?\n"
+                           "Please input a value between -100 and 100.\n"))
+    while userYCoord < -100 or userYCoord > 100:
+        print("The entered value is not in the specified range.\n"
+              "Please enter an integer between -100 and 100.")
+        userYCoord = int(input("Where would you like the circle to start on the Y axis?\n"))
     return userYCoord
 
 main()
