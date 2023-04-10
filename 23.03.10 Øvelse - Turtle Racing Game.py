004242015482052a8c5e880362301d3ed909deae
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-04-10 16:18:09 +0200

    Added exercise description to Python Racing game

---
+++ 23.03.10 Øvelse - Turtle Racing Game.py
@@ -0,0 +1,11 @@
+# Try to make a racing game using the turtle module in Python.
+# Imagine a number of turtles lined up beside each other.
+# At the other end of the screen is the goal line.
+# You may now bet on a turtle (take an input from the user)
+# Next, in a loop, each turtle moves a random number of pixels
+# The first turtle that reaches the line is the winner.
+# Check if the player (user) won or lost.
+# Add-ons could be:
+# Sound and graphics.
+
+
