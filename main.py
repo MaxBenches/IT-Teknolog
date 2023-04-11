7185fb7c69628efb33b0728ae1e354873e302e25
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-04-11 16:14:26 +0200

    Tilføjet noget bajs

---
+++ main.py
@@ -0,0 +1,15 @@
+from machine import Pin
+from time import sleep
+
+led00 = Pin("LED", Pin.OUT)    # onboard LED
+#led01 = Pin("GPIO0", Pin.OUT)
+
+led00.off()
+#led01.off()
+
+while True:
+	led00.on()
+	sleep(0.3)
+	led00.off()
+	sleep(0.3)
+
