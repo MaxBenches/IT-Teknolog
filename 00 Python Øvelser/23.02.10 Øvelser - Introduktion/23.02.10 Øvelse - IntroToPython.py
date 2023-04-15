86caf9a5df9edf7dea678612f026175f464fcdc5
MaxBenches <113772275+MaxBenches@users.noreply.github.com>
2023-04-15 21:48:55 +0200

    Updated library

--- 00 Python Øvelser/23.02.10 Øvelser - Introduktion/23.02.10 Øvelse - IntroToPython.py
+++ 00 Python Øvelser/23.02.10 Øvelser - Introduktion/23.02.10 Øvelse - IntroToPython.py
@@ -4,5 +4,5 @@ name = "Max"
 age = int("28")
 
 print("Hello IT-Technology student " + name + "!")
-print("I can see from your age that youy are born in the year: "+str(2023-age) + ".")
+print("I can see from your age that you are born in the year: "+str(2023-age) + ".")
 print("A big welcome to you you " +name+" from Business Academy Aarhus!")
